c975a6f3b2a03e55a599e15a57b77f0aa78c6f42
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-11-29 00:04:58 +0900

    Create TMDM_Dataloader.py

---
+++ 줄거리 기반 추천 시스템(Bert)/TMDM_Dataloader.py
@@ -0,0 +1,10 @@
+from torch.utils.data import DataLoader
+
+loader_dataset = TMDM_Dataset(movie_data = movies, 
+                              max_len = 80,
+                              tokenizer = BERT_Tokenizer)
+
+train_dataloader = DataLoader(loader_dataset, 
+                             batch_size = 16, 
+                             shuffle = True, 
+                             drop_last = True)
